5823741ce0f8d054618745e62b7f06ba47f0033f
Hadeus7712 <anibro.gl@gmail.com>
2023-02-18 20:03:03 +0300

    0.1.1

---
+++ app/src/main/java/com/example/personalcounter/Months.java
@@ -0,0 +1,16 @@
+package com.example.personalcounter;
+
+public enum Months {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
